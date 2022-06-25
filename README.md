8866e8e9752d37b70524441c18b3d397d6bd3c01
Jorge Martinez <martinezjam@gmail.com>
2022-06-25 18:38:27 -0300

    Iniciando

---
+++ README.md
@@ -0,0 +1,6 @@
+Consigna: Crear una web que permite ver los datos de algunos de tus familiares, guardados en un BD.
+
+1-Deberá tener un template, una vista y un modelo (como mínimo, pueden usar más)
+2-La clase del modelo, deberá guardar mínimo un número, una cadena y una fecha (puede guardar más cosas)
+3-Se deberán crear como mínimo 3 familiares
+4-Los familiares se deben ver desde la web.
---
+++ jjjmvt/views.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from multiprocessing import context
+from unittest import loader
+from django.http import HttpResponse
+from django.template import Template
+from django.template import Context
+
+def inicio(request):
+    return HttpResponse('<h1>MVT</h1>')
+
+def pepe(request):
+    # template1 = loader.get_template('home.html')
+    # render1=template1.render({'nombre':'Momia','edad':4598})
+    archivo = open(r'D:\Curso Python 2022\31085\mvt_JFJMJZ\templates\home.html') #v1
+    template1= Template(archivo.read())#v1
+    archivo.close()#v1
+    
+    context1= Context() #v1
+    render1=template1.render(context1) #v1
+    
+    return HttpResponse(f'Mi template: {render1}')

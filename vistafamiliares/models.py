632373e8e3aae1c921ae2acd2d60b01186b89d11
Jorge Martinez <martinezjam@gmail.com>
2022-06-25 22:39:24 -0300

    Model generado y migration ejecutado

--- vistafamiliares/models.py
+++ vistafamiliares/models.py
@@ -2,7 +2,7 @@ from mailbox import NoSuchMailboxError
 from django.db import models
 
 # Create your models here.
-class familia(models.Model):
-    nombre=models.CharField()
+class Familia(models.Model):
+    nombre=models.CharField(max_length=30)
     edad=models.IntegerField()
     fecha_nacimiento=models.DateField(null=True)
